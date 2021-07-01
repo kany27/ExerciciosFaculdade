7364c28efa588927212d14d1697de21e9efa2c52
kanydian <kanydianestevescanedo@yahoo.com>
2021-07-01 20:22:53 -0300

    Procurando conteudo de um arquivo e reescrevendo em outro.

---
+++ C++/exercicio2.cpp
@@ -0,0 +1,24 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  fstream arq, out;
+  string line;
+
+  arq.open("teste.txt", ios::in);
+  out.open("novo.txt", ios::out);
+
+  if(!arq)
+    cout << "Arquivo não pode ser aberto" << endl;
+
+  while(getline(arq, line)){
+    if((line.find("192.168.10.222")) != string::npos) {
+      out << line << endl;
+    }
+  }
+
+  return 0;
+}
