2e850e274007b3e57874687e8f73c0dfa2f8eaa5
kanydian <kanydianestevescanedo@yahoo.com>
2021-10-17 13:47:35 -0300

    17-10-21

--- C/exercicio17.c
+++ C/exercicio17.c
@@ -5,22 +5,21 @@ int main() {
   int maior, menor;
 
   for (unsigned int i = 0; i < 10; i++) {
-    printf("\nDigite um numero: ");
+    printf("Informe um número: ");
     scanf("%d", &lista[i]);
   }
 
   maior = lista[0];
   menor = lista[0];
 
-  for (int i = 0; i < 10; i++) {
+  for (int i = 0; i < 10; i ++) {
     if (lista[i] > maior)
       maior = lista[i];
     else if (lista[i] < menor)
-      menor = lista[i];
+      menor = lista[i];  
   }
 
-  printf("\nO maior numero da lista: %d", maior);
-  printf("\nO menor numero da lista: %d", menor);
-
+  printf("O maior número da lista é: %d\n", maior);
+  printf("O menor número da lista é: %d\n", menor);
   return 0;
 }
